2c2176eeb0a3c4349ee2163df943454b9c18dfbe
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2022-01-10 20:02:00 -0600

    templete inicial del proyecto de node con express

---
+++ models/Server.js
@@ -0,0 +1,35 @@
+const express = require( 'express' );
+const cors = require( 'cors' );
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.port;
+
+        //* Rutas de la applicación
+        this.routes();
+    }
+
+    middlewares() {
+        // * Cors
+        this.app.use( cors() );
+
+        // * Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // * Directorio Público
+        this.app.use( express.static( 'public' ) );
+    }
+
+    routes() {
+
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log( 'Servidor corriendo en el puerto: ', this.port );
+        });
+    }
+}
+
+module.exports = Server;
