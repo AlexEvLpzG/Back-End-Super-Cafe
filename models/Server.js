5207d98864cc6ff2029a64738b88c59c98db2fb5
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2022-01-11 15:16:43 -0600

    agregamos la conexión de Mongo DB

--- models/Server.js
+++ models/Server.js
@@ -1,13 +1,24 @@
 const express = require( 'express' );
 const cors = require( 'cors' );
+const { dbConnection } = require('../database/config');
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.port;
 
-        //* Rutas de la applicación
+        // * Conectar a base de datos
+        this.conectarDB();
+
+        // * Middlewares
+        this.middlewares();
+
+        // * Rutas de la applicación
         this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
     }
 
     middlewares() {
@@ -27,7 +38,7 @@ class Server {
 
     listen() {
         this.app.listen( this.port, () => {
-            console.log( 'Servidor corriendo en el puerto: ', this.port );
+            console.log( 'Servidor corriendo en el puerto:', this.port );
         });
     }
 }
