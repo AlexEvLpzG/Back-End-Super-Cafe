77e40fd495b59c17bd16e50250c0221194316114
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2022-01-13 16:36:37 -0600

    agregamos la ruta para iniciar sesión

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,53 @@
 const { request, response } = require( 'express' );
+const bcryptjs = require('bcryptjs');
 
-const login = ( req = request, res = response ) => {
-    res.json({
-        msg: 'Login ok'
-    });
+const { generarJWT } = require('../helpers/generar-jwt');
+const Usuario = require( '../models/usuario' );
+
+const login = async( req = request, res = response ) => {
+    const { correo, password } = req.body;
+
+    try {
+        // * Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+
+        if( !usuario ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+
+        // * Si el usuario está activo
+        if( !usuario.estado ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            });
+        }
+
+        // * Verificar la contraseña
+        const validPassword = bcryptjs.compareSync( password, usuario.password );
+
+        if( !validPassword ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+
+        // * Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            msg: 'Login ok',
+            usuario,
+            token
+        });
+    } catch (error) {
+        console.log( error );
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 }
-
 
 module.exports = {
     login
