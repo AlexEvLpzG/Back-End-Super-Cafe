09c0fef9398a12cb4056724d8d93c968553998c6
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2022-01-13 17:48:27 -0600

    modificamos las importaciones de nuestros middlewares

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,9 +2,11 @@ const { Router } = require( 'express' );
 const { check } = require( 'express-validator' );
 
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
-const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
-const { esAdminRole, tieneRole } = require('../middlewares/validar-roles');
+
+// const { validarCampos } = require('../middlewares/validar-campos');
+// const { validarJWT } = require('../middlewares/validar-jwt');
+// const { esAdminRole, tieneRole } = require('../middlewares/validar-roles');
+const { validarCampos, validarJWT, esAdminRole, tieneRole } = require( '../middlewares' );
 
 const { crearUsuario, actualizarUsuario, obtenerUsuarios, eliminarUsuario } = require('../controllers/usuarios');
 
