77ba933b51355e7b968629c001370f77c9ff376f
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-04 17:09:26 -0600

    Creamos el Modelo para los Productos

--- models/index.js
+++ models/index.js
@@ -1,10 +1,12 @@
 const Categoria = require( './categoria' );
+const Producto = require( './producto' );
 const Role = require( './role' );
 const Server = require( './Server' );
 const Usuario = require( './usuario' );
 
 module.exports = {
     Categoria,
+    Producto,
     Role,
     Server,
     Usuario
