bef6cfa68589091427ef91a01a011d57c934f86d
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-06 20:17:53 -0600

    Agregamos la busqueda de usuarios

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,9 +1,56 @@
 const { response } = require( 'express' );
+const { ObjectId } = require( 'mongoose' ).Types;
+
+const { Usuario } = require( '../models' );
+
+const coleccioneaPermitidas = [
+    'usuarios',
+    'categorias',
+    'productos',
+    'roles'
+];
+
+const buscarUsarios = async( termino = '', res = response ) => {
+    const esMongoID = ObjectId.isValid( termino );
+
+    if( esMongoID ) {
+        const usuario = await Usuario.findById( termino );
+        return res.status(201).json({
+            results: ( usuario ) ? [ usuario ] : []
+        });
+    } 
+
+    const regex = new RegExp( termino, 'i' );
+    const usuarios = await Usuario.find({ 
+        $or: [{ nombre: regex }, { correo: regex }],
+        $and: [{ estado: true }]
+     });
+
+    res.status(201).json({
+        results: usuarios
+    });
+}
 
 const buscar = ( req, res = response ) => {
     const { coleccion, termino } = req.params;
 
-    res.json({ coleccion, termino });
+    if( coleccioneaPermitidas.includes( coleccion ) ) {
+
+    } else {
+        return res.status(400).json({ msg: `Las colecciones permitidas son: ${ coleccioneaPermitidas}` });
+    }
+
+    switch( coleccion ) {
+        case 'usuarios':
+            buscarUsarios( termino, res );
+            break;
+        case 'categorias':
+            break;
+        case 'productos':
+            break;
+        default: 
+            res.status(500).json({ msg: 'No se a implementado discha busqueda' });
+    }
 }
 
 module.exports = { buscar };
