ef6ac059125a0ff5b61a72a09a683c2f58e29994
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-08 23:21:00 -0600

    creamos nuestro helper para subir-archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,31 +1,14 @@
-const path = require( 'path' );
 const { response } = require( 'express' );
+const { subirArchivo } = require( '../helpers/subir-archivos' );
 
-const cargarArchivo = ( req, res = response ) => {
+const cargarArchivo = async( req, res = response ) => {
     if ( !req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
         return res.status(400).json({ msg: 'No hay archivos que subir.' });
     }
 
-    const { archivo } = req.files;
-    const nombreCortado = archivo.name.split( '.' );
-    const extension = nombreCortado[ nombreCortado.length - 1 ];
+    const nombre = await subirArchivo( req.files );
 
-    // * Validar la extensión
-    const extensionesValidas = [ 'png', 'jpg', 'jpeg', 'gif' ];
-
-    if( !extensionesValidas.includes( extension ) ) {
-        return res.status(400).json({ msg: `La extensión ${ extension } no es valida, ${ extensionesValidas }`});
-    }
-
-    const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
-
-    archivo.mv( uploadPath, ( err ) => {
-        if (err) {
-            return res.status(500).json({ err });
-        }
-
-        res.json({ msg: 'File uploaded to ' + uploadPath });
-    });
+    res.status(200).json({ nombre });
 }
 
 module.exports = { 
