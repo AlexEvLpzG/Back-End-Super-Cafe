00b83aaadf3967f2992fabc3729fafb71bb6479b
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-08 22:46:10 -0600

    Agregamos la validación de las extensiones de los archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -7,6 +7,15 @@ const cargarArchivo = ( req, res = response ) => {
     }
 
     const { archivo } = req.files;
+    const nombreCortado = archivo.name.split( '.' );
+    const extension = nombreCortado[ nombreCortado.length - 1 ];
+
+    // * Validar la extensión
+    const extensionesValidas = [ 'png', 'jpg', 'jpeg', 'gif' ];
+
+    if( !extensionesValidas.includes( extension ) ) {
+        return res.status(400).json({ msg: `La extensión ${ extension } no es valida, ${ extensionesValidas }`});
+    }
 
     const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
 
