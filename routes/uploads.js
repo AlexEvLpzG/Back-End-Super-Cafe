2387f45a458a44473b50b3fe85f495ad8be0621e
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-09 21:59:39 -0600

    definimos la ruta para actualizar las imagenes de Usuarios y Productos

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,10 +2,19 @@ const { Router } = require( 'express' );
 const { check } = require( 'express-validator' );
 
 const { validarCampos } = require( '../middlewares/validar-campos' );
-const { cargarArchivo } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
+const { coleccionesPermitidas } = require('../helpers/db-validators');
 
 const router = Router();
 
 router.post( '/', cargarArchivo );
+
+router.put( '/:coleccion/:id',
+    [
+        check( 'id', 'El Id no es valido' ).isMongoId(),
+        check( 'coleccion' ).custom( c => coleccionesPermitidas( c, [ 'usuarios', 'productos' ]) ),
+        validarCampos
+    ], actualizarImagen
+)
 
 module.exports = router;
