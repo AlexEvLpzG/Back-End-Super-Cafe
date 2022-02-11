5c5757e151a0744180c4960511100bb618b9c00a
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-10 22:18:59 -0600

    Agregamos las funciones para eliminar imagenes.

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,3 +1,6 @@
+const path = require( 'path' );
+const fs = require( 'fs' );
+
 const { response } = require( 'express' );
 const { subirArchivo } = require( '../helpers/subir-archivos' );
 const { Usuario, Producto } = require( '../models' );
@@ -34,6 +37,14 @@ const actualizarImagen = async( req , res = response ) => {
             return res.status(500).json({ msg: 'No se ha agregado la colección' });
     }
 
+    // * Limpia imágenes previas
+    if( modelo.img ) {
+        // * Hay que borrar la imagen del servidor
+        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
+        if( fs.existsSync( pathImagen ) ) {
+            fs.unlinkSync( pathImagen );
+        }
+    }
     const nombre = await subirArchivo( req.files, undefined, coleccion );
     modelo.img = nombre;
 
